e4187cae384d889b33a5307a17b6756796927318
medium2000cls <medium2000cls@yandex.ru>
2023-11-28 11:30:01 +0300

    2

--- MyDynamoClass/ObjectToBlock.cs
+++ MyDynamoClass/ObjectToBlock.cs
@@ -14,6 +14,7 @@ using Colors = Autodesk.AutoCAD.Colors;
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.Civil.DataShortcuts;
 using Autodesk.DesignScript.Runtime;
+using DBObject = Autodesk.Civil.DatabaseServices.DBObject;
 using ds = Autodesk.DesignScript.Geometry;
 using dyn = Autodesk.AutoCAD.DynamoNodes;
 using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
@@ -26,22 +27,22 @@ namespace MyDynamoClass
         public static string HandleToBlock(Autodesk.AutoCAD.DynamoNodes.Document doc_dyn, List<string> object_handles,
             string block_name)
         {
-            Document ac_doc = doc_dyn.AcDocument;
-            Database ac_db = ac_doc.Database;
-            Editor ac_ed = ac_doc.Editor;
+            Document acDoc = doc_dyn.AcDocument;
+            Database acDb = acDoc.Database;
+            Editor ac_ed = acDoc.Editor;
 
             string block_handle = "";
 
-            using (DocumentLock acLckDoc = ac_doc.LockDocument())
+            using (DocumentLock acLckDoc = acDoc.LockDocument())
             {
-                using (Transaction acTrans = ac_db.TransactionManager.StartTransaction())
+                using (Transaction acTrans = acDb.TransactionManager.StartTransaction())
                 {
                     try
                     {
                         // Открыть таблицу блоков для чтения
-                        BlockTable acBlkTbl = acTrans.GetObject(ac_db.BlockTableId, OpenMode.ForRead) as BlockTable;
-                        
-                        
+                        BlockTable acBlkTbl = acTrans.GetObject(acDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+
                         // создаем новое определение блока
                         BlockTableRecord acBlkTblRec = new BlockTableRecord();
                         acBlkTblRec.Name = block_name;
@@ -50,7 +51,7 @@ namespace MyDynamoClass
 
                         // добавляем его в таблицу блоков
                         ObjectId btrId = acBlkTbl.Add(acBlkTblRec);
-                        acTrans.AddNewlyCreatedDBObject(acBlkTblRec, true);
+                        //acTrans.AddNewlyCreatedDBObject(acBlkTblRec, true);
 
                         ObjectIdCollection ids = new ObjectIdCollection();
 
@@ -58,13 +59,14 @@ namespace MyDynamoClass
                         {
                             //Получаем ObjectId
                             long obj_handle = Convert.ToInt64(handle, 16);
-                            ObjectId id = ac_db.GetObjectId(false, new Handle(obj_handle), 0);
+                            ObjectId id = acDb.GetObjectId(false, new Handle(obj_handle), 0);
                             ids.Add(id);
                             //Entity entity = acTrans.GetObject(id, OpenMode.ForWrite) as Entity;
                         }
-                        
+
                         acBlkTblRec.AssumeOwnershipOf(ids);
-                        //acTrans.AddNewlyCreatedDBObject(entity, true);
+                        acTrans.AddNewlyCreatedDBObject(acBlkTblRec, true);
+
                         block_handle = acBlkTblRec.Handle.ToString();
                         acTrans.Commit();
                     }
@@ -74,7 +76,105 @@ namespace MyDynamoClass
                     }
                 }
             }
+
             return block_handle;
+        }
+
+        public static List<string> HandleToBlocks(Autodesk.AutoCAD.DynamoNodes.Document doc_dyn,
+            List<List<string>> object_handles,
+            List<string> block_names)
+        {
+            List<string> blockHandles = new List<string>();
+            if (object_handles.Count == block_names.Count)
+            {
+                int listCount = object_handles.Count;
+                for (int i = 0; i < listCount; ++i)
+                {
+                    var handle = HandleToBlock(doc_dyn, object_handles[i], block_names[i]);
+                    blockHandles.Add(handle);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Object count not equal to name count.");
+            }
+
+            return blockHandles;
+        }
+
+        public static List<string> HandleToBlockReference(Autodesk.AutoCAD.DynamoNodes.Document doc_dyn,
+            List<string> object_handles)
+        {
+            Document acDoc = doc_dyn.AcDocument;
+            Database acDb = acDoc.Database;
+            Editor acEd = acDoc.Editor;
+
+            List<string> blockReferenceHandle = new List<string>();
+
+            using (DocumentLock acLckDoc = acDoc.LockDocument())
+            {
+                using (Transaction acTrans = acDb.TransactionManager.StartTransaction())
+                {
+                    try
+                    {
+                        // Открыть таблицу блоков для чтения
+                        BlockTable acBlkTbl = acTrans.GetObject(acDb.BlockTableId, OpenMode.ForWrite) as BlockTable;
+
+                        // теперь создадим экземпляр блока на чертеже
+                        // получаем пространство модели
+                        BlockTableRecord btrModelSpace =
+                            acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as
+                                BlockTableRecord;
+                        foreach (var handle in object_handles)
+                        {
+                            long objHandle = Convert.ToInt64(handle, 16);
+                            var s = new Handle(objHandle);
+                            s.ToString();
+                            ObjectId id = acDb.GetObjectId(false, new Handle(objHandle), 0);
+                            // создаем новый экземпляр блока на основе его определения
+                            BlockReference brRefBlock = new BlockReference(Point3d.Origin, id);
+
+                            // добавляем экземпляр блока в базу данных пространства модели
+                            ObjectId blockReferenceId = btrModelSpace.AppendEntity(brRefBlock);
+                            blockReferenceHandle.Add(blockReferenceId.Handle.ToString());
+                            acTrans.AddNewlyCreatedDBObject(brRefBlock, true);
+                        }
+
+                        acTrans.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        blockReferenceHandle.Add(e.ToString());
+                    }
+                }
+            }
+
+            return blockReferenceHandle;
+        }
+
+        public static void HandleToSaveBlock(Autodesk.AutoCAD.DynamoNodes.Document doc_dyn, List<string> object_handles, string full_name)
+        {
+            Document acDoc = doc_dyn.AcDocument;
+            Database acDb = acDoc.Database;
+            using (DocumentLock acLckDoc = acDoc.LockDocument()) {
+                using (Transaction acTrans = acDb.TransactionManager.StartTransaction()) {
+                    BlockTable acBlkTbl = acTrans.GetObject(acDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+                    
+                    Database acDbNew = new Database();
+                    ObjectIdCollection blockIds = new ObjectIdCollection();
+
+                    foreach (var handle in object_handles)
+                    {
+                        Handle objHandle = new Handle(Convert.ToInt64(handle, 16));
+                        ObjectId id_block = acDb.GetObjectId(false, objHandle, 0);
+                        blockIds.Add(id_block);
+                    }
+                    IdMapping mapping = new IdMapping();
+                    acDbNew.WblockCloneObjects(blockIds,acDbNew.CurrentSpaceId, mapping, DuplicateRecordCloning.Replace, false);
+                    acDbNew.SaveAs(full_name, DwgVersion.Current);
+                    acTrans.Commit();
+                }
+            }
         }
     }
 }
